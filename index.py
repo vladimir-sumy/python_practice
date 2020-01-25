90f33b14ae0f7353664e40cf086cd8491b824082
Vladimir Lysak <vladimir.sumy@gmail.com>
2020-01-25 11:56:51 +0200

    Add string section

--- index.py
+++ index.py
@@ -9,3 +9,12 @@ c = 3
 r = 3
 print( c, id(c))
 print(r, id(r))
+
+#strings
+h = 'Hello '
+w = 'world! '
+phrase = h + w
+print(phrase * 4)
+print( phrase, type(phrase))
+
+print(a, b, c, r, h, w, phrase)
